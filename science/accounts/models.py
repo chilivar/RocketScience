00181674f2a84c3c9adca5859236c11f80defcb1
Maxim Plugov <107909771+chilivar@users.noreply.github.com>
2024-11-05 22:43:55 +0500

    добавил модель RegisterUser

--- science/accounts/models.py
+++ science/accounts/models.py
@@ -1,3 +1,15 @@
+
 from django.db import models
 
-# Create your models here.
+class RegisterUser(models.Model):
+    
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    contact = models.CharField(max_length=30)  
+    email = models.EmailField(unique=True)
+    
+
+
+
+
+    
